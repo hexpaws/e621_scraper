86c4e46cf76867d64e88fcb87c0ca59580481222
hxpw <hexapaws@gmail.com>
2017-03-27 20:54:02 +0200

    Update e621_scraper.py
    added option for pools

--- e621_scraper.py
+++ e621_scraper.py
@@ -1,36 +1,34 @@
-from sys import platform as _os
 import requests
-import re
-import wget
-import time
 import argparse
+import shutil
+import time
+import sys
+import re
 import os
 
 resp = requests.get
 foundImg = []
 foundLinks = []
+foundID = []
 debugging = True
 workdir = os.getcwd()
 baseUrl = "https://e621.net/post/index/"
 
 def motd():
-	print "					  "
-	print "		   __ ___  __ "
-	print "  	  / /|__ \/_ |"
-	print "	 ___ / /_   ) || |"
-	print " / _ \ '_ \ / / | |"
-	print "|  __/ (_) / /_ | |"
-	print " \___|\___/____||_|"
-	print "	 .:.Scraper.:.	  "
+	print "e621"
+	print ".:.Scraper.:.\n"
 
-def getImgz_e621(url):
+def getImgz_e621(url, counter=0):
 	
 	response = resp(url).text
 	linkz = re.compile(ur'"file_url":"https:\/\/static1.e621.net\/data\S{39}.[jpg|png|jpeg|gif]+')
-	loot = re.findall(linkz, response)
-	urlID  = args.search
+	idz = re.compile(ur'"id":\d{6,}')
+	linksLoot = re.findall(linkz, response)
+	idLoot = re.findall(idz, response)
+	urlID  = args.search or str(args.pool)
 
 	if not os.path.exists(urlID) or os.path.exists("../"+urlID):
+	
 			try:
 					os.mkdir(urlID)
 			except OSError as oserr_mkdir:
@@ -42,29 +40,41 @@ def getImgz_e621(url):
 	elif os.path.exists(urlID):
 			os.chdir(urlID)
 		 
-	for j in os.listdir("../"+urlID):
-			foundImg.append(j)
+	for ls in os.listdir("../"+urlID):
+			foundImg.append(ls)
 
-	for j in loot:
-		clean = j.replace('"file_url":"', '')
-		foundLinks.append(clean)
+	del(foundLinks[:])
+	for dirtyLink in linksLoot:
+		cleanLink = dirtyLink.replace('"file_url":"', '').encode('utf-8')
+		foundLinks.append(cleanLink)
 
-	for links in foundLinks:
-		try:  
-			loot2 = links[36:]
-			img_name = str(loot2)
-			print img_name
-			foundImg.index(img_name)
+	del(foundID[:])
+	for dirtyID in idLoot:
+		cleanID = dirtyID.replace('"id":', '').encode('utf-8')
+		foundID.append(cleanID)
+
+	IDpLink_Dict = dict(zip(foundID, foundLinks))
+	
+
+
+	for ID, LINK in IDpLink_Dict.items():
+		try:
+				foundImg.index(ID)
+				print foundID
 
 		except ValueError as valueerr:           
-				if debugging:
-					print("Value Error: {0}".format(valueerr))   
+					if debugging:
+						print("Value Error: {0}".format(valueerr))
 
-				print "input wget", links
-				print url
-				print (wget.download(links))
-				print "\n"		
-
+					
+					stream = resp(LINK, stream=True)
+					fileExtension = LINK[-4:]
+					start = time.time()
+					name = ID+fileExtension
+					with open(name, 'wb') as file:
+						shutil.copyfileobj(stream.raw, file)
+						counter += 1
+					print "Downloaded: {0} \t Count: {1} \t Seconds: {2:4f} \t Filesize: {3:d}KB \t".format(name, counter, (time.time()-start), (os.stat(name).st_size >> 10))
 
 	os.chdir(workdir)
 	if debugging:            
@@ -73,9 +83,28 @@ def getImgz_e621(url):
 parser = argparse.ArgumentParser()
 parser.add_argument("--search")
 parser.add_argument("--pages")
+parser.add_argument("--pool")
 args = parser.parse_args()
 
 motd()
-for j in range(1,int(args.pages)):
-	compiledUrl = baseUrl+str(j)+str("/")+args.search
-	getImgz_e621(compiledUrl)
+"""
+while resp():
+	for pool in range(5,9999):
+		compiledUrl = baseUrl.replace('post', 'pool').replace('index','show')+str('6960')+"?page="+str(pool)
+		print "URL: {0}".format(compiledUrl)
+		getImgz_e621(compiledUrl)
+"""	
+
+if args.search and args.pages:
+	for pages in range(1,int(args.pages)):
+		compiledUrl = baseUrl+str(pages)+str("/")+args.search
+		
+		print "URL: {0}".format(compiledUrl)
+		getImgz_e621(compiledUrl)
+
+if args.pool: 
+	while True:
+		for pool in range(1,9999):
+			compiledUrl = baseUrl.replace('post', 'pool').replace('index','show')+str(args.pool)+"?page="+str(pool)
+			print "URL: {0}".format(compiledUrl)
+			getImgz_e621(compiledUrl)
